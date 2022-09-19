06bbca774c153d43ec2f322f0fb92c53ede0e8ba
Erik Kerimov <erik.nextup@gmail.com>
2022-09-19 17:32:55 +0300

    Added StartBuildFarmConstructionResponsePacket

--- Packets/ServerToClient/IServerToClientPacket.cs
+++ Packets/ServerToClient/IServerToClientPacket.cs
@@ -4,6 +4,7 @@ namespace ServerPrototype.Shared.Packets.ServerToClient
 {
     [Union(0, typeof(LoginConfirmPacket))]
     [Union(1, typeof(LoginDenyPacket))]
+    [Union(2, typeof(StartBuildFarmConstructionResponsePacket))]
     public interface IServerToClientPacket : IEquatable<IServerToClientPacket>
     {
         string ToString();
---
+++ Packets/ServerToClient/StartBuildFarmConstructionResponsePacket.cs
@@ -0,0 +1,24 @@
+﻿using MessagePack;
+
+namespace ServerPrototype.Shared.Packets.ServerToClient
+{
+    [MessagePackObject]
+    public sealed class StartBuildFarmConstructionResponsePacket : IServerToClientPacket
+    {
+        [Key(0)]
+        public int BuildDurationSecond { get; set; }
+
+        public bool Equals(IServerToClientPacket other)
+        {
+            return Equals(other as object);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((StartBuildFarmConstructionResponsePacket)obj);
+        }
+    }
+}
